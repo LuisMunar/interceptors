9d656ecd6828dd21e4c630eff5e80cccaa28e7cf
LuisMunar <luis.emunarr@gmail.com>
2024-04-23 07:09:14 -0500

    Video 91 ready

---
+++ src/main/java/com/spring/boot/interceptors/interceptors/AppConfig.java
@@ -0,0 +1,23 @@
+package com.spring.boot.interceptors.interceptors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+  
+  @Autowired
+  @Qualifier("loadingTimeInterceptor")
+  private HandlerInterceptor loadingTimeInterceptor;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    // WebMvcConfigurer.super.addInterceptors(registry);
+    registry.addInterceptor(loadingTimeInterceptor);
+  }
+  
+}
---
+++ src/main/java/com/spring/boot/interceptors/interceptors/InterceptorsApplication.java
@@ -0,0 +1,11 @@
+package com.spring.boot.interceptors.interceptors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterceptorsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InterceptorsApplication.class, args);
+	}
+}
