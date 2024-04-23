109010fdd611c0b8f366d70168a8d292fd458b23
LuisMunar <luis.emunarr@gmail.com>
2024-04-23 09:33:39 -0500

    Video 97 ready

--- src/main/java/com/spring/boot/interceptors/interceptors/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/spring/boot/interceptors/interceptors/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,6 @@
 package com.spring.boot.interceptors.interceptors.interceptors;
 
+import java.util.Map;
 import java.util.Random;
 
 import org.slf4j.Logger;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -31,6 +34,18 @@ public class LoadingTimeInterceptor implements HandlerInterceptor {
     Random random = new Random();
     int delay = random.nextInt(1000);
     Thread.sleep(delay);
+
+    if (delay > 500) {
+      Map<String, Object> model = Map.of("message", "many long timed");
+      
+      ObjectMapper objectMapper = new ObjectMapper();
+      String jsonString = objectMapper.writeValueAsString(model);
+
+      response.setContentType("application/json");
+      response.setStatus(500);
+      response.getWriter().write(jsonString);
+      return false;
+    }
 
     return true;
   }
